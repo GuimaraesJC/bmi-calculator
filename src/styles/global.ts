91544a81dfb57771d812449a4950caac647565b3
Jean Carlos Costa <guimaraes.jean95@gmail.com>
2023-05-17 21:19:27 -0300

    Installing styled-components and configuring global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 16px;
+  }
+
+  *,
+  *:after,
+  *:before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  a, button {
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;
