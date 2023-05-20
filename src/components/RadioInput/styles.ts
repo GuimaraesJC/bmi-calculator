0e5f04336ec110b0a22a5280e6d6149861ce00cf
Jean Carlos Costa <guimaraes.jean95@gmail.com>
2023-05-20 00:57:13 -0300

    Identation fix

--- src/components/RadioInput/styles.ts
+++ src/components/RadioInput/styles.ts
@@ -32,8 +32,8 @@ export const Input = styled.input`
     transform: scale(0);
     transition: 120ms transform ease-in-out;
     box-shadow:
-    inset 1rem 1rem ${({ theme }) => theme.colors.blue},
-    0rem 0rem 0px 8px rgba(52, 95, 246, 0.15);
+      inset 1rem 1rem ${({ theme }) => theme.colors.blue},
+      0rem 0rem 0px 8px rgba(52, 95, 246, 0.15);
   }
 
   &[type="radio"]:checked::before {
