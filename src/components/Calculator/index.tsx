69a6442bde21c0182d965fb68136a526534bd81f
Jean Carlos Costa <guimaraes.jean95@gmail.com>
2023-05-21 14:57:16 -0300

    Creates logic to toggle between two sets of inputs for units

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import Text from '../Text'
 import RadioInput from '../RadioInput'
 import Input from '../Input'
@@ -5,18 +7,40 @@ import Input from '../Input'
 import * as S from './styles'
 
 function Calculator() {
+  const [isMetric, setIsMetric] = useState<boolean>(true);
+
   return (
     <S.Wrapper>
       <Text renderAs="span" type="HeadingM">
         Enter your details below
       </Text>
       <S.UnitSelectionContainer>
-        <RadioInput label="Metric" name="system" checked />
-        <RadioInput label="Imperial" name="system" />
+        <RadioInput
+          label="Metric"
+          name="system"
+          onClick={() => setIsMetric(true)}
+          defaultChecked
+        />
+        <RadioInput
+          label="Imperial"
+          name="system"
+          onClick={() => setIsMetric(false)}
+        />
       </S.UnitSelectionContainer>
       <S.InputsContainer>
-        <Input label="Height" unit="cm" />
-        <Input label="Weight" unit="kg" />
+        {isMetric ? (
+          <>
+            <Input label="Height" unit="cm" />
+            <Input label="Weight" unit="kg" />
+          </>
+        ) : (
+          <>
+            <Input label="Height" unit="ft" />
+            <Input unit="in" />
+            <Input label="Weight" unit="st" />
+            <Input unit="lbs" />
+          </>
+        )}
       </S.InputsContainer>
       <S.ResultContainer>
         Results
