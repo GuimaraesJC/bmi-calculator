3898606b3a0022af9af5eba887d2462c4c009613
Jean Carlos Costa <guimaraes.jean95@gmail.com>
2023-05-23 20:45:44 -0300

    Finishes calculator structure

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -5,9 +5,11 @@ import RadioInput from '../RadioInput'
 import Input from '../Input'
 
 import * as S from './styles'
+import { defaultTheme } from '../../styles/theme'
 
 function Calculator() {
   const [isMetric, setIsMetric] = useState<boolean>(true);
+  const [hasResult, setHasResult] = useState<boolean>(true);
 
   return (
     <S.Wrapper>
@@ -42,8 +44,55 @@ function Calculator() {
           </>
         )}
       </S.InputsContainer>
-      <S.ResultContainer>
-        Results
+      <S.ResultContainer hasResult={hasResult}>
+        {hasResult ? (
+          <>
+            <S.ResultCalculationContainer>
+              <Text
+                renderAs="span"
+                type="BodyM-Bold"
+                color={defaultTheme.colors.white}
+              >
+                Your BMI is...
+              </Text>
+              <Text
+                renderAs="span"
+                type="HeadingXL"
+                color={defaultTheme.colors.white}
+              >
+                23.4
+              </Text>
+            </S.ResultCalculationContainer>
+            <div>
+              <Text
+                renderAs="p"
+                type="BodyS"
+                color={defaultTheme.colors.white}
+              >
+                Your BMI suggests you’re a healthy weight.
+                Your ideal weight is between 63.3kgs - 85.2kgs.
+              </Text>
+            </div>
+          </>
+        ) : (
+          <S.NoResultTextContainer>
+            <Text
+              renderAs="span"
+              type="HeadingM"
+              color={defaultTheme.colors.white}
+            >
+              Welcome!
+            </Text>
+            <Text
+              renderAs="p"
+              type="BodyS"
+              color={defaultTheme.colors.white}
+            >
+              Enter your height and weight and you’ll see your BMI result here
+            </Text>
+          </S.NoResultTextContainer>
+        )}
+
       </S.ResultContainer>
     </S.Wrapper>
   )
--- src/components/Calculator/styles.ts
+++ src/components/Calculator/styles.ts
@@ -6,7 +6,7 @@ export const Wrapper = styled.div`
   gap: 2rem;
   background-color: white;
   width: 35.25rem;
-  height: 30.25rem;
+  min-height: 28rem;
   padding: 2rem;
   box-shadow: 1rem 2rem 3.5rem rgba(143, 174, 207, 0.25);
   border-radius: 1rem;
@@ -24,4 +24,34 @@ export const InputsContainer = styled.div`
   gap: 1.5rem;
 `
 
-export const ResultContainer = styled.div``
+interface ResultContainerProps {
+  hasResult: boolean
+}
+
+export const ResultContainer = styled.div<ResultContainerProps>`
+  width: 100%;
+  height: ${({ hasResult }) => hasResult ? '10.375rem' : '8.125rem'};
+  display: ${({ hasResult }) => hasResult ? 'grid' : 'block'};
+
+  ${({ hasResult }) => hasResult && `
+    grid-template-columns: 1fr 1fr;
+    gap: 1.5rem;
+    align-items: center;
+  `};
+
+  background: linear-gradient(90deg, #345FF7 0%, #587DFF 100%);
+  border-radius: 1rem 100rem 100rem 1rem;
+  padding: 2rem;
+`
+
+export const ResultCalculationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`
+
+export const NoResultTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
